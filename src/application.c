462bafb39b0de174c07d49d56cf1e9c48a76b300
zdenyv <zdenyv@users.noreply.github.com>
2022-12-01 18:21:13 +0100

    added climate module

--- src/application.c
+++ src/application.c
@@ -4,6 +4,34 @@
 #define TEMPERATURE_PUB_VALUE_CHANGE 0.2f
 #define TEMPERATURE_UPDATE_INTERVAL (1 * 1000)
 
+#define SERVICE_INTERVAL_INTERVAL (60 * 60 * 1000)
+#define BATTERY_UPDATE_INTERVAL   (60 * 60 * 1000)
+
+#define UPDATE_SERVICE_INTERVAL            (5 * 1000)
+#define UPDATE_NORMAL_INTERVAL             (10 * 1000)
+#define BAROMETER_UPDATE_SERVICE_INTERVAL  (1 * 60 * 1000)
+#define BAROMETER_UPDATE_NORMAL_INTERVAL   (5 * 60 * 1000)
+
+#define TEMPERATURE_TAG_PUB_NO_CHANGE_INTEVAL (15 * 60 * 1000)
+#define TEMPERATURE_TAG_PUB_VALUE_CHANGE 0.2f
+
+#define HUMIDITY_TAG_PUB_NO_CHANGE_INTEVAL (15 * 60 * 1000)
+#define HUMIDITY_TAG_PUB_VALUE_CHANGE 5.0f
+
+#define LUX_METER_TAG_PUB_NO_CHANGE_INTEVAL (15 * 60 * 1000)
+#define LUX_METER_TAG_PUB_VALUE_CHANGE 25.0f
+
+#define BAROMETER_TAG_PUB_NO_CHANGE_INTEVAL (15 * 60 * 1000)
+#define BAROMETER_TAG_PUB_VALUE_CHANGE 20.0f
+
+struct {
+    event_param_t temperature;
+    event_param_t humidity;
+    event_param_t illuminance;
+    event_param_t pressure;
+
+} params;
+
 // LED instance
 twr_led_t led;
 bool led_state = false;
@@ -298,13 +326,91 @@ void twr_radio_node_on_led_strip_thermometer_set(uint64_t *id, float *temperatur
     twr_scheduler_plan_now(led_strip.update_task_id);
 }
 
+void climate_module_event_handler(twr_module_climate_event_t event, void *event_param)
+{
+    (void) event_param;
+
+    float value;
+
+    if (event == TWR_MODULE_CLIMATE_EVENT_UPDATE_THERMOMETER)
+    {
+        if (twr_module_climate_get_temperature_celsius(&value))
+        {
+            if ((fabs(value - params.temperature.value) >= TEMPERATURE_TAG_PUB_VALUE_CHANGE) || (params.temperature.next_pub < twr_scheduler_get_spin_tick()))
+            {
+                twr_radio_pub_temperature(TWR_RADIO_PUB_CHANNEL_R1_I2C0_ADDRESS_DEFAULT, &value);
+                params.temperature.value = value;
+                params.temperature.next_pub = twr_scheduler_get_spin_tick() + TEMPERATURE_TAG_PUB_NO_CHANGE_INTEVAL;
+            }
+        }
+    }
+    else if (event == TWR_MODULE_CLIMATE_EVENT_UPDATE_HYGROMETER)
+    {
+        if (twr_module_climate_get_humidity_percentage(&value))
+        {
+            if ((fabs(value - params.humidity.value) >= HUMIDITY_TAG_PUB_VALUE_CHANGE) || (params.humidity.next_pub < twr_scheduler_get_spin_tick()))
+            {
+                twr_radio_pub_humidity(TWR_RADIO_PUB_CHANNEL_R3_I2C0_ADDRESS_DEFAULT, &value);
+                params.humidity.value = value;
+                params.humidity.next_pub = twr_scheduler_get_spin_tick() + HUMIDITY_TAG_PUB_NO_CHANGE_INTEVAL;
+            }
+        }
+    }
+    else if (event == TWR_MODULE_CLIMATE_EVENT_UPDATE_LUX_METER)
+    {
+        if (twr_module_climate_get_illuminance_lux(&value))
+        {
+            if (value < 1)
+            {
+                value = 0;
+            }
+            if ((fabs(value - params.illuminance.value) >= LUX_METER_TAG_PUB_VALUE_CHANGE) || (params.illuminance.next_pub < twr_scheduler_get_spin_tick()) ||
+                    ((value == 0) && (params.illuminance.value != 0)) || ((value > 1) && (params.illuminance.value == 0)))
+            {
+                twr_radio_pub_luminosity(TWR_RADIO_PUB_CHANNEL_R1_I2C0_ADDRESS_DEFAULT, &value);
+                params.illuminance.value = value;
+                params.illuminance.next_pub = twr_scheduler_get_spin_tick() + LUX_METER_TAG_PUB_NO_CHANGE_INTEVAL;
+            }
+        }
+    }
+    else if (event == TWR_MODULE_CLIMATE_EVENT_UPDATE_BAROMETER)
+    {
+        if (twr_module_climate_get_pressure_pascal(&value))
+        {
+            if ((fabs(value - params.pressure.value) >= BAROMETER_TAG_PUB_VALUE_CHANGE) || (params.pressure.next_pub < twr_scheduler_get_spin_tick()))
+            {
+                float meter;
+
+                if (!twr_module_climate_get_altitude_meter(&meter))
+                {
+                    return;
+                }
+
+                twr_radio_pub_barometer(TWR_RADIO_PUB_CHANNEL_R1_I2C0_ADDRESS_DEFAULT, &value, &meter);
+                params.pressure.value = value;
+                params.pressure.next_pub = twr_scheduler_get_spin_tick() + BAROMETER_TAG_PUB_NO_CHANGE_INTEVAL;
+            }
+        }
+    }
+}
+
+void switch_to_normal_mode_task(void *param)
+{
+    twr_module_climate_set_update_interval_thermometer(UPDATE_NORMAL_INTERVAL);
+    twr_module_climate_set_update_interval_hygrometer(UPDATE_NORMAL_INTERVAL);
+    twr_module_climate_set_update_interval_lux_meter(UPDATE_NORMAL_INTERVAL);
+    twr_module_climate_set_update_interval_barometer(BAROMETER_UPDATE_NORMAL_INTERVAL);
+
+    twr_scheduler_unregister(twr_scheduler_get_current_task_id());
+}
+
 void application_init(void)
 {
     // Initialize LED
     twr_led_init(&led, TWR_GPIO_LED, false, false);
     twr_led_set_mode(&led, TWR_LED_MODE_OFF);
 
-    twr_radio_init(TWR_RADIO_MODE_NODE_LISTENING);
+    twr_radio_init(TWR_RADIO_MODE_NODE_SLEEPING);   // Zdeny: změna na SLEEPING z fw pro climate module
 
     // Initialize button
     twr_button_init(&button, TWR_GPIO_BUTTON, TWR_GPIO_PULL_DOWN, false);
@@ -317,6 +423,15 @@ void application_init(void)
     twr_tag_temperature_set_update_interval(&temperature, TEMPERATURE_UPDATE_INTERVAL);
     twr_tag_temperature_set_event_handler(&temperature, temperature_tag_event_handler, &temperature_event_param);
 
+    // Initialize climate module
+    twr_module_climate_init();
+    twr_module_climate_set_event_handler(climate_module_event_handler, NULL);
+    twr_module_climate_set_update_interval_thermometer(UPDATE_SERVICE_INTERVAL);
+    twr_module_climate_set_update_interval_hygrometer(UPDATE_SERVICE_INTERVAL);
+    twr_module_climate_set_update_interval_lux_meter(UPDATE_SERVICE_INTERVAL);
+    twr_module_climate_set_update_interval_barometer(BAROMETER_UPDATE_SERVICE_INTERVAL);
+    twr_module_climate_measure_all_sensors();
+
     // Initialize power module
     twr_module_power_init();
 
@@ -325,7 +440,9 @@ void application_init(void)
 
     led_strip.update_task_id = twr_scheduler_register(led_strip_update_task, NULL, 0);
 
-    twr_radio_pairing_request("power-controller", FW_VERSION);
+    twr_radio_pairing_request("vyz-fve-baterry-monitor", FW_VERSION);
+
+    twr_scheduler_register(switch_to_normal_mode_task, NULL, SERVICE_INTERVAL_INTERVAL);
 
     twr_led_pulse(&led, 2000);
 }
